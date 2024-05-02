e91c7b25cfec61b266adffef6a933d8b45ef2d25
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-02 16:18:02 +0530

    client handler class done

--- src/main/java/org/example/client/ClientHandler.java
+++ src/main/java/org/example/client/ClientHandler.java
@@ -27,6 +27,55 @@ public class ClientHandler implements Runnable {
 
     @Override
     public void run() {
+        String clientMessage;
 
+        while(socket.isConnected()) {
+            try {
+                clientMessage = bufferedReader.readLine();
+                broadcastMessage(clientMessage);
+            }
+            catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String message) {
+        for (ClientHandler clientHandler : clientHandlers) {
+            try {
+                if (!clientHandler.clientUsername.equals(clientUsername)) {
+                    clientHandler.bufferedWriter.write(message);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            }
+            catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUsername + " has left the chat!");
+    }
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        removeClientHandler();
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+            if (bufferedWriter != null) {
+                bufferedWriter.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
