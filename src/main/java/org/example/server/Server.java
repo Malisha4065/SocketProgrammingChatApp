cdc848b5d43d15b9e39d9d5d2c7620b41b5f112f
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-02 15:38:14 +0530

    server class created

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,4 +1,49 @@
 package org.example.server;
 
+import org.example.client.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has arrived.");
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        }
+        catch(IOException e) {
+
+        }
+    }
+
+    public void stopServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
 }
