fc9645d4c0f71bfff4dca0efa7583cdee71f3675
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-08 01:47:15 +0530

    basic setup

--- src/main/java/org/malisha/chatappjavafx/HelloApplication.java
+++ src/main/java/org/malisha/chatappjavafx/HelloApplication.java
@@ -11,8 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        stage.setMinWidth(640);
+        stage.setMinHeight(480);
+        stage.setTitle("Chat App");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/malisha/chatappjavafx/HelloController.java
+++ src/main/java/org/malisha/chatappjavafx/HelloController.java
@@ -2,6 +2,10 @@ package org.malisha.chatappjavafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.malisha.server.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class HelloController {
     @FXML
@@ -10,5 +14,15 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void initialize() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+            Server server = new Server(serverSocket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
