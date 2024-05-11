39b66381e2b29e3a8a1b2eeefee95dabcc215e5d
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2024-05-11 21:55:44 +0530

    login page css

--- src/main/java/org/malisha/chatappjavafx/LoginController.java
+++ src/main/java/org/malisha/chatappjavafx/LoginController.java
@@ -2,19 +2,36 @@ package org.malisha.chatappjavafx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
     @FXML
     private TextField usernameField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        usernameField.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                try {
+                    enterChat(new ActionEvent()); // Create a synthetic ActionEvent and pass it
+                } catch (IOException e) {
+                    e.printStackTrace(); // Handle IOException if necessary
+                }
+            }
+        });
+    }
 
     @FXML
     private void enterChat(ActionEvent event) throws IOException {
@@ -34,8 +51,8 @@ public class LoginController {
 
             Scene scene = new Scene(root, 640, 480);
             scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
-            stage.setMinWidth(640);
-            stage.setMinHeight(480);
+            stage.setMinWidth(1080);
+            stage.setMinHeight(720);
             stage.setTitle("Chat App");
             stage.setScene(scene);
             stage.setResizable(false);
@@ -46,4 +63,6 @@ public class LoginController {
             System.out.println("Please enter a valid username.");
         }
     }
+
+
 }
